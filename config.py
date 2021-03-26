19a8821f27ffdff5251557630d0b0b71c8c07455
Anderson Ferneda <aferneda@uolinc.com>
2021-03-26 08:19:42 -0300

    Mudança na estrutura de arquivos

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'bdl1ba91ba-189s-you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'per.db')
+        'sqlite:///' + os.path.join(basedir, 'quiz.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
