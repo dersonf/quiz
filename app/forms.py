0d955a3742530e885b13580923d71cfdadc44571
Anderson Ferneda <aferneda@uolinc.com>
2021-04-02 00:32:38 -0300

    Desenvolvimento geral

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField, SubmitField, SelectField, RadioField
+from wtforms import (
+    StringField,
+    BooleanField,
+    SubmitField,
+    SelectField,
+    RadioField,
+    IntegerField,
+)
 from wtforms.validators import DataRequired
 
 
-class CadastroForm(FlaskForm):
+class BotaoConfirma(FlaskForm):
+    submit = SubmitField('Confirma')
+
+
+class CadastroForm(BotaoConfirma):
     pergunta = StringField('Pergunta', [DataRequired()])
     resposta1 = StringField('Resposta 1', [DataRequired()])
     resposta2 = StringField('Resposta 2', [DataRequired()])
@@ -20,19 +31,26 @@ class CadastroForm(FlaskForm):
         (33, 'Média'),
         (66, 'Difícil'),
         ])
-    submit = SubmitField('Cadastrar')
 
 
 class PerguntaForm(FlaskForm):
-    # resposta = RadioField('Resposta', choices=[
-    #     ('resposta1', 'Resposta 1'),
-    #     ('resposta2', 'Resposta 2'),
-    #     ('resposta3', 'Resposta 3'),
-    #     ('resposta4', 'Resposta 4')
-    #     ])
     submit = SubmitField('Responder')
 
-    # def __init__(self, respostas: list = None, *args, **kwargs):
-    #     super().__init__(*args, **kwargs)
-    #     self.resposta.choices = respostas
 
+class ConsultaForm(BotaoConfirma):
+    pergunta_id = IntegerField('ID', [DataRequired()])
+
+
+class EditaPerguntaForm(BotaoConfirma):
+    pergunta = StringField('Pergunta', [DataRequired()])
+    dificuldade = IntegerField('Dificuldade', [DataRequired()])
+
+
+class EditaRespostaForm(BotaoConfirma):
+    resposta = StringField('Resposta', [DataRequired()])
+    correta = BooleanField('Correta')
+
+
+class NomeForm(FlaskForm):
+    nome = StringField('Nome', [DataRequired()])
+    iniciar = SubmitField('Iniciar')
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,26 @@
-from flask import render_template, redirect, url_for, flash, request, session
-from app.forms import CadastroForm, PerguntaForm
+from flask import (
+    render_template,
+    redirect,
+    url_for,
+    flash,
+    request,
+    session,
+    abort,
+)
+from app.forms import (
+    CadastroForm,
+    PerguntaForm,
+    ConsultaForm,
+    EditaPerguntaForm,
+    EditaRespostaForm,
+    NomeForm,
+)
 from app import app, db
 from app.models import Perguntas, Respostas
 from random import choice, sample
 from wtforms import RadioField
 from wtforms.validators import DataRequired
+import jinja2
 
 @app.route('/')
 def index():
@@ -47,19 +63,25 @@ def pergunta(pergunta):
     form = PerguntaForm()
     if form.validate_on_submit():
         return redirect(url_for('corrigir', resposta=form.resposta.data))
-    return render_template('pergunta.html', title='Pergunta', pergunta=pergunta, form=form)
+    return render_template('pergunta.html', title='Pergunta:', pergunta=pergunta, form=form)
 
 
 @app.route('/gera_pergunta')
 def gera_pergunta():
     id_perguntas, opcoes = [], []
+    session['perguntas'] = session.get('perguntas')
     # Pega todas as perguntas
     perguntas = Perguntas.query.all()
-    # Coloca o id das perguntas em uma lista
+    # Coloca o id das perguntas em uma lista exceto as já perguntas
     for id in perguntas:
-        id_perguntas.append(id.id)
+        if id.id not in session['perguntas']:
+            id_perguntas.append(id.id)
+    if not id_perguntas:
+        return redirect('/corrigir/fim')
     # Sorteia uma pergunta
     pergunta = Perguntas.query.get(choice(id_perguntas))
+    # Coloca na sessão perguntas já feitas
+    session['perguntas'].append(pergunta.id)
     # Pega as respostas da pergunta
     respostas = Respostas.query.filter_by(pergunta_id=pergunta.id)
     for resposta in respostas:
@@ -71,9 +93,82 @@ def gera_pergunta():
 
 @app.route('/corrigir/<resposta>')
 def corrigir(resposta):
-    valida = Respostas.query.get(resposta)
-    if valida.correta == True:
-        return render_template('acerto.html', title='Correção')
-    else:
+    if resposta == 'fim':
         session.clear()
-        return render_template('errou.html', title='Correção')
+        return render_template('index.html', title='Acabaram as perguntas. 500.000 pontos!!!')
+    valida = Respostas.query.get(resposta)
+    pergunta = Perguntas.query.get(valida.pergunta_id)
+    if valida.correta == True:
+        session['pontos'] = session.get('pontos') + 1000
+        
+        if pergunta.dificuldade >= 0:
+            pergunta.dificuldade -= 1
+            db.session.commit()
+        return render_template('acerto.html', title='Correto!!!')
+    else:
+        if pergunta.dificuldade <= 100:
+            pergunta.dificuldade += 1
+            db.session.commit()
+        pontos = session.get('pontos')
+        nome = session.get('nome')
+        session.clear()
+        return render_template('errou.html', title='Errou!!!', nome=nome, pontos=pontos)
+
+
+@app.route('/consulta', methods=['GET', 'POST'])
+def consulta():
+    form = ConsultaForm()
+    if form.validate_on_submit():
+        try:
+            pergunta = Perguntas.query.get(form.pergunta_id.data)
+            respostas = Respostas.query.filter_by(pergunta_id=pergunta.id)
+            return render_template('consulta.html', title='Editar', form=form, pergunta=pergunta, respostas=respostas)
+        except AttributeError:
+            flash("ID não existe.")
+        else:
+            return redirect(url_for('consulta'))
+    try:
+        return render_template('consulta.html', title='Editar', form=form)
+    except jinja2.exceptions.UndefinedError:
+        flash("ID não existe.")
+        return redirect(url_for('consulta'))
+
+@app.route('/editar/<tipo>/<id>', methods=['GET', 'POST'])
+def editar(tipo, id):
+    if tipo == 'pergunta':
+        pergunta = Perguntas.query.get(id)
+        form = EditaPerguntaForm()
+    elif tipo == 'resposta':
+        resposta = Respostas.query.get(id)
+        form = EditaRespostaForm()
+    if form.validate_on_submit():
+        if tipo == 'pergunta':
+            pergunta.pergunta = form.pergunta.data
+            pergunta.dificuldade = form.dificuldade.data
+        elif tipo == 'resposta':
+            resposta.resposta = form.resposta.data
+            resposta.correta = form.correta.data
+        db.session.commit()
+        flash('Pergunta atualizada com sucesso!')
+        return redirect(url_for('consulta'))
+    if tipo == 'pergunta':
+        form.pergunta.data = pergunta.pergunta
+        form.dificuldade.data = pergunta.dificuldade
+    elif tipo == 'resposta':        
+        form.resposta.data = resposta.resposta
+        form.correta.data = resposta.correta
+    return render_template('edita_pergunta.html', form=form)
+
+
+@app.route('/iniciar', methods=['GET', 'POST'])
+def iniciar():
+    form = NomeForm()
+    if form.validate_on_submit():
+        session['nome'] = form.nome.data.title()
+        session['pontos'] = 0
+        session['perguntas'] = []
+        return redirect(url_for('gera_pergunta'))
+    else:
+        print(form.errors)
+    return render_template('iniciar.html', title='Iniciar', form=form)
+
